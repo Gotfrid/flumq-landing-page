9d1fee7646652d0318bc9b06ce5ddaceaec949f7
Pavel Demin <paveldemin@me.com>
2022-10-02 11:00:53 +0800

    feat: add logos

--- components/StartPage.tsx
+++ components/StartPage.tsx
@@ -1,10 +1,17 @@
 import styles from "../styles/StartPage.module.css";
+import logoText from "../public/logo-text.svg";
+import logoSymbol from "../public/logo-symbol.svg";
+import Image from "next/image";
 
 const StartPage = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.textLogo}>Logo1</div>
-      <div className={styles.imageLogo}>Logo2</div>
+      <div className={styles.textLogo}>
+        <Image src={logoText} height="50px" />
+      </div>
+      <div className={styles.imageLogo}>
+        <Image src={logoSymbol} height="50px" />
+      </div>
       <h1 className={styles.title}>Your guide to moving abroad</h1>
       <p className={styles.subtitle}>
         Choose country, access multiple guides, ask questions and share your
