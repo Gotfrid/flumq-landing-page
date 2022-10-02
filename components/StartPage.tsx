863f70e80273be20331b71f44eee844607222426
Pavel Demin <paveldemin@me.com>
2022-10-02 12:24:46 +0800

    feat: bottom group

--- components/StartPage.tsx
+++ components/StartPage.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 import styles from "../styles/StartPage.module.css";
 import logoText from "../public/logo-text.svg";
 import logoSymbol from "../public/logo-symbol.svg";
-import Image from "next/image";
+import appStoreGray from "../public/app-store-gray.svg";
+import googlePlayGray from "../public/google-play-gray.svg";
+import iphone14 from "../public/iphone14.svg";
+import scrollIcon from "../public/scroll-icon.svg";
+import qrCode from "../public/qr-code.svg";
+import diagonalArrow from "../public/diagonal-arrow.svg";
 
 const StartPage = () => {
   return (
@@ -18,9 +25,32 @@ const StartPage = () => {
         experience
       </p>
       <div className={styles.integrations}>
-        <div>App store</div>
-        <div>iPhone</div>
-        <div>Google play</div>
+        <div>
+          <Image src={appStoreGray} />
+        </div>
+        <div>
+          <Image src={iphone14} />
+        </div>
+        <div>
+          <Image src={googlePlayGray} />
+        </div>
+      </div>
+      <div className={styles.bottomGroup}>
+        <div className={styles.bottomCurvature} />
+        <div className={styles.bottomFiller} />
+      </div>
+      <div className={styles.scrollIconContainer}>
+        <Image src={scrollIcon} />
+      </div>
+      <div className={styles.qrContainer}>
+        <div className={styles.qrText}>
+          <p>Scan to</p>
+          <p>download</p>
+        </div>
+        <div className={styles.diagonalArrow}>
+          <Image src={diagonalArrow} />
+        </div>
+        <Image src={qrCode} />
       </div>
     </div>
   );
