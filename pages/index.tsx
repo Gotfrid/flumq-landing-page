e7955a9e7d1a2a030b7f2e9d435988a2e67d93e2
Pavel Demin <paveldemin@me.com>
2022-10-07 11:59:34 +0800

    chore: dump everythin into one file for ease of dev

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,63 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 
-import StartPage from "../components/StartPage";
-import styles from "../styles/Home.module.css";
+import Head from "next/head";
+import Image from "next/image";
+
+import logoText from "../public/logo-text.svg";
+import logoSymbol from "../public/logo-symbol.svg";
+import appStoreGray from "../public/app-store-gray.svg";
+import googlePlayGray from "../public/google-play-gray.svg";
+import iphone14 from "../public/iphone14.svg";
+import scrollIcon from "../public/scroll-icon.svg";
+import qrCode from "../public/qr-code.svg";
+import diagonalArrow from "../public/diagonal-arrow.svg";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Flumq</title>
         <meta name="description" content="Your guide to moving abroad" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <StartPage />
-      </main>
-    </div>
+      <div className="container">
+        <div className="textLogo">
+          <Image src={logoText} height="50px" />
+        </div>
+        <div className="imageLogo">
+          <Image src={logoSymbol} height="50px" />
+        </div>
+        <h1 className="title">Your guide to moving abroad</h1>
+        <p className="subtitle">
+          Choose country, access multiple guides, ask questions and share your
+          experience
+        </p>
+        <div className="integrations">
+          <div>
+            <Image src={appStoreGray} />
+          </div>
+          <div className="iphoneContainer">
+            <Image src={iphone14} />
+          </div>
+          <div>
+            <Image src={googlePlayGray} />
+          </div>
+        </div>
+        <div className="scrollIconContainer">
+          <Image src={scrollIcon} />
+        </div>
+        <div className="qrContainer">
+          <div className="qrText">
+            <p>Scan to</p>
+            <p>download</p>
+          </div>
+          <div className="diagonalArrow">
+            <Image src={diagonalArrow} />
+          </div>
+          <Image src={qrCode} />
+        </div>
+      </div>
+    </>
   );
 };
 
