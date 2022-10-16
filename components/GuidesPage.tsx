2d95ccbd09a400c54c4fc8c0b539d4e341e3d3ad
Pavel Demin <paveldemin@me.com>
2022-10-16 20:52:51 +0800

    feat: start responsive guides

--- components/GuidesPage.tsx
+++ components/GuidesPage.tsx
@@ -11,17 +11,17 @@ const guideTabs = ["Description", "Timeline", "Stats"];
 const backgroundCircleMapping = [
   {
     tabName: "Description",
-    className: "backgroundCirclePink",
+    className: styles.backgroundCirclePink,
     component: () => <DescriptionContent />,
   },
   {
     tabName: "Timeline",
-    className: "backgroundCirclePurple",
+    className: styles.backgroundCirclePurple,
     component: () => <TimelineContent />,
   },
   {
     tabName: "Stats",
-    className: "backgroundCircleTeal",
+    className: styles.backgroundCircleTeal,
     component: () => <StatsContent />,
   },
 ];
@@ -44,33 +44,40 @@ const GuidesPage = () => {
   )?.component;
 
   return (
-    <section className={classNames("page", styles.guides)}>
-      <div className={styles.blackBackground} />
-      <span className={styles.accessText}>Access</span>
-      <span className={styles.guidesText}>Step-by-Step Guides</span>
-      <div className={styles.guidesContainer}>
+    <section className={classNames("page", styles.guidesPage)}>
+      <div className={styles.hero}>
+        <h3>Access</h3>
+        <h1>Step-by-Step Guides</h1>
+      </div>
+
+      <div className={styles.widgetContainer}>
         <div className={styles.btnGroup} onClick={handleGuideSwitch}>
           {guideTabs.map((tabName) => (
             <button
               key={tabName}
-              className={tabName === activeGuideTab ? "btnActive" : ""}
+              className={tabName === activeGuideTab ? styles.btnActive : ""}
             >
               {tabName}
             </button>
           ))}
         </div>
+      </div>
+
+      {/* <div className={styles.guidesContainer}>
         <div className={styles.iphoneFrame}>
           {guideComponent && guideComponent()}
         </div>
-      </div>
-      <span className={styles.description}>
+      </div> */}
+
+      <div className={styles.description}>
         <span className={styles.highlightLight}>No more uncertainties</span>
-        <span className={styles.explainTextWhite}>
+        <span>
           {" "}
           Choose a path that shows what to do next, track progress, share
           timeline, or create your own guide.
         </span>
-      </span>
+      </div>
+
       <div className={styles.backgroundCircleContainer}>
         <div className={circleClassname} />
         <div className={circleClassname} />
