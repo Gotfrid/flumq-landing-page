d20c1ab0d79b5d4c146c6c7b09f11becea214374
Pavel Demin <paveldemin@me.com>
2022-10-21 13:52:17 +0800

    feat: confine width - destinations page

--- components/DestinationsPage.tsx
+++ components/DestinationsPage.tsx
@@ -10,28 +10,30 @@ import styles from "../styles/DestinationsPage.module.scss";
 const DestinationsPage = () => {
   return (
     <section className={classNames("page", styles.destinationPage)}>
-      <div className={styles.destinations}>
-        {/* // TODO: build a component with each card as a component */}
-        <Image src={destinations} />
-        <div className={styles.icons}>
-          <div>
-            <Image src={searchIcon} />
-          </div>
-          <div>
-            <Image src={settingsIcon} />
+      <div className={styles.container}>
+        <div className={styles.destinations}>
+          {/* // TODO: build a component with each card as a component */}
+          <Image src={destinations} />
+          <div className={styles.icons}>
+            <div>
+              <Image src={searchIcon} />
+            </div>
+            <div>
+              <Image src={settingsIcon} />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className={styles.hero}>
-        <h3>Find your</h3>
-        <h1>Destination</h1>
-        <div className={styles.heroDescription}>
-          <span className={styles.highlightDark}>Know your options</span>
-          <span>
-            {" "}
-            Search countries by filters, compare and make your choice.
-          </span>
+        <div className={styles.hero}>
+          <h3>Find your</h3>
+          <h1>Destination</h1>
+          <div className={styles.heroDescription}>
+            <span className={styles.highlightDark}>Know your options</span>
+            <span>
+              {" "}
+              Search countries by filters, compare and make your choice.
+            </span>
+          </div>
         </div>
       </div>
     </section>
