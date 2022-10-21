e79a5cb685c4ec428ecc61d86a0b3cd8c17e01ed
Pavel Demin <paveldemin@me.com>
2022-10-21 13:55:27 +0800

    feat: confine width - community

--- components/CommunityPage.tsx
+++ components/CommunityPage.tsx
@@ -8,20 +8,22 @@ import styles from "../styles/CommunityPage.module.scss";
 const CommunityPage = () => {
   return (
     <section className={classNames("page", styles.communityPage)}>
-      <div className={styles.hero}>
-        <h3>Join the</h3>
-        <h1>Community</h1>
-        <div className={styles.heroDescription}>
-          <span className={styles.highlightDark}>You're not alone</span>
-          <span>
-            {" "}
-            Meet new people, join discussions, share your experience, ask
-            questions, and more.
-          </span>
+      <div className={styles.container}>
+        <div className={styles.hero}>
+          <h3>Join the</h3>
+          <h1>Community</h1>
+          <div className={styles.heroDescription}>
+            <span className={styles.highlightDark}>You're not alone</span>
+            <span>
+              {" "}
+              Meet new people, join discussions, share your experience, ask
+              questions, and more.
+            </span>
+          </div>
         </div>
-      </div>
-      <div className={styles.communityContainer}>
-        <Image src={community3x} layout="fill" objectFit="contain" />
+        <div className={styles.communityContainer}>
+          <Image src={community3x} layout="fill" objectFit="contain" />
+        </div>
       </div>
     </section>
   );
