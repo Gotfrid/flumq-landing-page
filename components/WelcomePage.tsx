122fd1692a6b779f7cdaa5c75a8847f190ae42ea
Pavel Demin <paveldemin@me.com>
2022-10-21 13:34:39 +0800

    feat: confine max-width - welcome

--- components/WelcomePage.tsx
+++ components/WelcomePage.tsx
@@ -14,53 +14,55 @@ import StoreButton from "./Common/StoreButton";
 const WelcomPage = () => {
   return (
     <section className={classNames("page", styles.welcomePage)}>
-      <div className={styles.hero}>
-        <div className={styles.textLogo}>
-          <Image src={logoText} height="50px" />
+      <div className={styles.container}>
+        <div className={styles.hero}>
+          <div className={styles.textLogo}>
+            <Image src={logoText} height="50px" />
+          </div>
+          <div className={styles.imageLogo}>
+            <Image src={logoSymbol} height="50px" />
+          </div>
+          <h1 className={styles.title}>Your guide to moving abroad</h1>
+          <div className={styles.subtitle}>
+            Choose country, access multiple guides, ask questions and share your
+            experience.
+          </div>
         </div>
-        <div className={styles.imageLogo}>
-          <Image src={logoSymbol} height="50px" />
-        </div>
-        <h1 className={styles.title}>Your guide to moving abroad</h1>
-        <div className={styles.subtitle}>
-          Choose country, access multiple guides, ask questions and share your
-          experience.
-        </div>
-      </div>
 
-      <div className={styles.integrations}>
-        <StoreButton store="apple" color="gray" />
-        <div className={styles.iphone}>
-          <Image src={iphone14} />
-        </div>
-        <StoreButton store="google" color="gray" />
-      </div>
-
-      {/* TODO: there must be a better way? */}
-      <div className={styles.integrationsMobile}>
-        <div className={styles.stores}>
+        <div className={styles.integrations}>
           <StoreButton store="apple" color="gray" />
+          <div className={styles.iphone}>
+            <Image src={iphone14} />
+          </div>
           <StoreButton store="google" color="gray" />
         </div>
-        <div className={styles.iphone}>
-          <Image src={iphone14} />
-        </div>
-      </div>
 
-      <div className={styles.scrollIconContainer}>
-        <Image src={scrollIcon} />
-      </div>
+        {/* TODO: there must be a better way? */}
+        <div className={styles.integrationsMobile}>
+          <div className={styles.stores}>
+            <StoreButton store="apple" color="gray" />
+            <StoreButton store="google" color="gray" />
+          </div>
+          <div className={styles.iphone}>
+            <Image src={iphone14} />
+          </div>
+        </div>
 
-      <div className={styles.qrContainer}>
-        <div className={styles.qrText}>
-          <p>Scan to</p>
-          <p>download</p>
+        <div className={styles.scrollIconContainer}>
+          <Image src={scrollIcon} />
         </div>
-        <div className={styles.diagonalArrow}>
-          <Image src={diagonalArrow} />
-        </div>
-        <div className={styles.qrImage}>
-          <Image src={qrCode} />
+
+        <div className={styles.qrContainer}>
+          <div className={styles.qrText}>
+            <p>Scan to</p>
+            <p>download</p>
+          </div>
+          <div className={styles.diagonalArrow}>
+            <Image src={diagonalArrow} />
+          </div>
+          <div className={styles.qrImage}>
+            <Image src={qrCode} />
+          </div>
         </div>
       </div>
     </section>
