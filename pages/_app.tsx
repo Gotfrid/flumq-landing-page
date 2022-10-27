bd6caec447c97b25afa1dfe7a5c10b1129240612
Pavel Demin <paveldemin@me.com>
2022-10-27 10:59:46 +0800

    chore: add vercel analytics to the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import type { AppProps } from "next/app";
+
+import { Analytics } from "@vercel/analytics/react";
+
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
