fb70f767e80046e4057ccbe2d919aec89877c0a2
Pavel Demin <paveldemin@me.com>
2022-11-20 13:19:08 +0800

    feat: split community into sub-images

--- components/CommunityPage.tsx
+++ components/CommunityPage.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 import classNames from "classnames";
 
 import community3x from "../public/assets/community3x.png";
+import usercard1 from "../public/assets/user-cards/card1.png";
+import usercard2 from "../public/assets/user-cards/card2.png";
+import usercard3 from "../public/assets/user-cards/card3.png";
+import usercard4 from "../public/assets/user-cards/card4.png";
 
 import styles from "../styles/CommunityPage.module.scss";
 
@@ -22,7 +26,21 @@ const CommunityPage = () => {
           </div>
         </div>
         <div className={styles.communityContainer}>
-          <Image src={community3x} layout="fill" objectFit="contain" />
+          <div className={styles.main}>
+            <Image src={community3x} layout="fill" objectFit="contain" />
+          </div>
+          <div className={styles.usercard1}>
+            <Image src={usercard1} />
+          </div>
+          <div className={styles.usercard2}>
+            <Image src={usercard2} />
+          </div>
+          <div className={styles.usercard3}>
+            <Image src={usercard3} />
+          </div>
+          <div className={styles.usercard4}>
+            <Image src={usercard4} />
+          </div>
         </div>
       </div>
     </section>
