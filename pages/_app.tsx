13ce4a0c09e9324b395a016892244708231379c0
Pavel Demin <paveldemin@me.com>
2022-12-04 12:45:06 +0800

    chore: remove google analytics until better times

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,12 @@
-import type { AppProps, NextWebVitalsMetric } from "next/app";
+import type { AppProps } from "next/app";
 
 import { Analytics } from "@vercel/analytics/react";
-import { event, GoogleAnalytics } from "nextjs-google-analytics";
 
 import "../styles/globals.scss";
-
-export function reportWebVitals(metric: NextWebVitalsMetric) {
-  const { id, name, label, value } = metric;
-  event(name, {
-    category: label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
-    value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
-    label: id, // id unique to current page load
-    nonInteraction: true, // avoids affecting bounce rate.
-  });
-}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* tag id is sourced from env variable */}
-      <GoogleAnalytics />
       <Component {...pageProps} />
       <Analytics />
     </>
