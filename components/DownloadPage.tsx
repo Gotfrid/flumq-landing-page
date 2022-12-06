6a61bc1fa009d6020ed48e2dcbae66b21fb46b74
Pavel Demin <paveldemin@me.com>
2022-12-06 11:55:27 +0800

    reafactor: create a SocialBar component

--- components/DownloadPage.tsx
+++ components/DownloadPage.tsx
@@ -2,19 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 import classNames from "classnames";
 
-import qrCode from "../public/assets/qr-code.svg";
 import appIcon from "../public/logo/app-icon.svg";
 import mailIcon from "../public/icons/mail-icon.svg";
-import facebookIcon from "../public/icons/facebook-icon.svg";
-import instagramIcon from "../public/icons/instagram-icon.svg";
-import youtubeIcon from "../public/icons/youtube-icon.svg";
-import twitterIcon from "../public/icons/twitter-icon.svg";
-import linkedinIcon from "../public/icons/linkedin-icon.svg";
 import footerLogo from "../public/logo/footer-logo.svg";
 
 import styles from "../styles/DownloadPage.module.scss";
 import StoreButton from "./Common/StoreButton";
 import QrCode from "./Common/QrCode";
+import { SocialBar } from "./Icons/SocialBar";
 
 const DownloadPage = () => {
   return (
@@ -48,33 +43,7 @@ const DownloadPage = () => {
               </span>
               <a href="mailto:business@flumq.com">business@flumq.com</a>
             </div>
-            <ul className={styles.socials}>
-              <li>
-                <a href="https://www.facebook.com/flumq" target="_blank">
-                  <Image src={facebookIcon} alt="Facebook icon" />
-                </a>
-              </li>
-              <li>
-                <a href="https://www.instagram.com/flumq.app/" target="_blank">
-                  <Image src={instagramIcon} alt="Instagram icon" />
-                </a>
-              </li>
-              <li className={styles.youtube}>
-                <a href="https://www.youtube.com/channel/UCFrC1y1KCd8hz9HA6DN48bQ" target="_blank">
-                  <Image src={youtubeIcon} alt="Youtube icon" />
-                </a>
-              </li>
-              <li>
-                <a href="https://twitter.com/FlumqApp" target="_blank">
-                  <Image src={twitterIcon} alt="Twitter icon" />
-                </a>
-              </li>
-              <li>
-                <a href="https://www.linkedin.com/company/flumq" target="_blank">
-                  <Image src={linkedinIcon} alt="Linkedin icon" />
-                </a>
-              </li>
-            </ul>
+            <SocialBar />
           </div>
 
           <div className={styles.footerBorder} />
