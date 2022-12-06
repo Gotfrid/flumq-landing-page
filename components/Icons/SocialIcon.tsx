bf5a2e873b58337e65511d950ba83d95c2193382
Pavel Demin <paveldemin@me.com>
2022-12-06 11:27:17 +0800

    feat: create a component for social link

---
+++ components/Icons/SocialIcon.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import { FC } from "react";
+
+import styles from "./SocialIcon.module.scss";
+
+type SocialIconProps = {
+  href: string;
+  src: any;
+  alt: string;
+};
+
+export const SocialIcon: FC<SocialIconProps> = ({ href, src, alt }) => {
+  return (
+    <a href={href} target="_blank" className={styles.socialIcon}>
+      <Image src={src} alt={alt} />
+    </a>
+  );
+};
