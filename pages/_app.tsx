b3d56195f309d622289360b8e0ef3c6bc19e00b8
Pavel Demin <paveldemin@me.com>
2023-01-03 12:55:42 +0800

    feat: add invisible image to hack google preview

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
 import type { AppProps } from "next/app";
 
+import Image from "next/image";
 import { Analytics } from "@vercel/analytics/react";
+
+import screenshot from "../public/og-image.png";
 
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      {/* This is a hack to make Google doc load a nice pic */}
+      <div style={{ visibility: "hidden", height: 0 }}>
+        <Image src={screenshot} />
+      </div>
+
       <Component {...pageProps} />
       <Analytics />
     </>
